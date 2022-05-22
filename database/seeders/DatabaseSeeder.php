e171ace5d75e8cf569cbf5a91cb5efe19a508de9
Karīna Jakovina <karina.jakovina@gmail.com>
2022-05-22 18:04:31 +0300

    Recipes model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Recipe;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         Recipe::factory(10)->create();
     }
 }
